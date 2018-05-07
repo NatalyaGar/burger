52a2c00cbac32aa552a918d8efd1aae398389d26
NatalyaGar <natalya.garusova@gmail.com>
2018-05-07 09:52:34 -0600

    files and folders

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (id, burger_name) VALUES ("10","Cheese Burger");
+
+INSERT INTO burgers (id, burger_name) VALUES ("20", "Beef Becon Burger");
+INSERT INTO burgers (id, burger_name) VALUES ("30", "Chicken Burger");
